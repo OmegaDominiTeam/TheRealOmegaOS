2db254725fe11139e06dd2e772e3390b32bb2a44
cfgexe <vybeast02@gmail.com>
2021-12-09 20:20:10 +1100

    Add project files.

---
+++ OmegaOS/Kernel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace OmegaOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Welcome to OmegaOS");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
