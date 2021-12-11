e41f61759bc9ffc519d44cde435dd37d40ba2150
cfgexe <65036495+cfgexe@users.noreply.github.com>
2021-12-11 13:32:41 +1100

    Update Kernel.cs

--- OmegaOS/Kernel.cs
+++ OmegaOS/Kernel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
@@ -7,6 +7,8 @@ namespace OmegaOS
 {
     public class Kernel : Sys.Kernel
     {
+        static List<string> commandarr = new List<string>() { "help", "about" };
+        static string commandarrstring = "help, about";
         protected override void BeforeRun()
         {
             Console.WriteLine("Welcome to OmegaOS");
@@ -22,11 +24,24 @@ namespace OmegaOS
                     Console.WriteLine("OmegaOS v1.0.0");
                     Run();
                     break;
-                case "root":
-                    Console.Write("Password: ");
-                    Console.ReadLine();
-                    Run();
+                default:
+                    Console.WriteLine(input + " is not a valid command.");
+                    input = null;
                     break;
+                case "help":
+                    Console.WriteLine("Valid commands are " + commandarrstring + ".");
+                    break;
+                case "neofetch":
+                    Console.WriteLine(@"  ____                              ____   _____ 
+ / __ \                            / __ \ / ____|
+| |  | |_ __ ___   ___  __ _  __ _| |  | | (___  
+| |  | | '_ ` _ \ / _ \/ _` |/ _` | |  | |\___ \ 
+| |__| | | | | | |  __/ (_| | (_| | |__| |____) |
+ \____/|_| |_| |_|\___|\__, |\__,_|\____/|_____/ 
+                        __/ |                    
+                       |___/");
+                    break;
+
             }
         }
     }
